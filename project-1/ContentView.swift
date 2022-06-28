ad6c2f18ab4ae124b20e751c577d84f8e78f2de3
Rutul Malvaniya <rutul.malvaniya2000@gmail.com>
2022-06-28 19:48:17 -0400

    Initial Commit

---
+++ project-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-1
+//
+//  Created by Rutul Malvaniya on 2022-06-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project-1/project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  project_1App.swift
+//  project-1
+//
+//  Created by Rutul Malvaniya on 2022-06-28.
+//
+
+import SwiftUI
+
+@main
+struct project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
